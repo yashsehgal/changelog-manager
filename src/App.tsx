0a97b9db457147e469319d49b6d4e6862cf977d2
Yash Sehgal <yash.sehgal@rocketium.com>
2024-03-29 20:51:12 +0530

    chore: react app reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
-export default function App() {
-  return <div className="App-container"></div>;
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
